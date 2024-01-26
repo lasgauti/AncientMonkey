5321254d920a8eb129927166f89ab15367a082d5
lasgauti <157837206+lasgauti@users.noreply.github.com>
2024-01-26 13:55:11 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "AncientMonkey";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Add the Ancient Monkey to Bloons TD6";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "lasgauti"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "AncientMonkey"; // TODO add your repo name here, also in the download url in README.md
 }
