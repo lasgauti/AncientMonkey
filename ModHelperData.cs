3368914ba49da66cee540bb43327299fb34299fc
lasgauti <157837206+lasgauti@users.noreply.github.com>
2024-01-29 17:36:46 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace AncientMonkey;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "40.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "AncientMonkey";
 
     public const string Description = "Add the Ancient Monkey to Bloons TD6";
